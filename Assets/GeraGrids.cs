6767c425017b48fedd7706feb061b6797c50840d
Thomaz Botelho <thomazrb@gmail.com>
2015-04-04 18:14:40 -0300

    Gerando a Agua

---
+++ Assets/GeraGrids.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeraGrids : MonoBehaviour {
+
+	private int posX, posY;
+	public GameObject ObjetoAgua;
+
+	// Use this for initialization
+	void Start () {
+		for (posX = -10; posX <=10 ; posX++)
+			for (posY =-5; posY <=5; posY++)
+				Instantiate(ObjetoAgua, new Vector3(posX, posY, 0), Quaternion.identity);
+	
+	}
+}
