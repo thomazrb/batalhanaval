3e063c557cb5bdb1a3ae8820c9d07a7b942b812c
Thomaz Botelho <thomazrb@gmail.com>
2015-04-04 19:41:24 -0300

    Gerando grid de adversário e aumentando o tamanho para 15x15 para ficar bonito =D

--- Assets/Scripts/GeraGrids.cs
+++ Assets/Scripts/GeraGrids.cs
@@ -5,8 +5,12 @@ public class GeraGrids : MonoBehaviour {
 
 	private int posX, posY;
 
-	private float MapaJogadorX = -7;
-	private float MapaJogadorY = -2;
+	private float MapaJogadorX = -8;
+	private float MapaJogadorY = -3;
+
+	private float MapaAdversarioX = 1;
+	private float MapaAdversarioY = -3;
+
 	private float MapaX;
 	private float MapaY;
 
@@ -21,13 +25,25 @@ public class GeraGrids : MonoBehaviour {
 
 		MapaX = MapaJogadorX;
 		MapaY = MapaJogadorY;
-		for (posX = 1; posX <=10 ; posX++)
-		{	for (posY =1; posY <=10; posY++)
+		for (posX = 1; posX <=15 ; posX++)
+		{	for (posY =1; posY <=15; posY++)
 			{	
 				Instantiate(ObjetoGrid, new Vector3(MapaX, MapaY, -1), Quaternion.identity);
 				MapaY = MapaY + 0.5f;
 			}
 			MapaY = MapaJogadorY;
+			MapaX = MapaX + 0.5f;
+		}
+
+		MapaX = MapaAdversarioX;
+		MapaY = MapaAdversarioY;
+		for (posX = 1; posX <=15 ; posX++)
+		{	for (posY =1; posY <=15; posY++)
+			{	
+				Instantiate(ObjetoGrid, new Vector3(MapaX, MapaY, -1), Quaternion.identity);
+				MapaY = MapaY + 0.5f;
+			}
+			MapaY = MapaAdversarioY;
 			MapaX = MapaX + 0.5f;
 		}
 	}
