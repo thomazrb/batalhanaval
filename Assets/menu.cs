b1f9c85d9c3485b12e3065066b33e9942ebff542
Thomaz Botelho <thomazrb@gmail.com>
2015-04-04 17:39:31 -0300

    Menu principal :D

---
+++ Assets/menu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+
+	public void Sair () {
+		Application.Quit();
+	}
+
+	public void Novo () {
+		Application.LoadLevel("jogo");
+	}
+}
